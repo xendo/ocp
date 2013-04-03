eb8e3b452d5fd409766f5ffad9f79e8e6971191e
Jerzy Zagorski <xendoo@gmail.com>
2013-04-03 21:27:20 +0200

    CrazyEnum

---
+++ DeclarationsAndAccessControl/src/com/sabre/ocp/CrazyEnum.java
@@ -0,0 +1,33 @@
+package com.sabre.ocp;
+
+public enum CrazyEnum {
+
+	INSTANCE(1)
+	,CUSTOM_INSTANCE(2) {
+		public int getSomeLocalField() {
+			return 3;
+		}
+	};
+	
+	private volatile transient int someLocalField;
+	
+	CrazyEnum(int someLocalField) {
+		this.someLocalField = someLocalField;
+	}
+	
+	public int getSomeLocalField() {
+		return someLocalField;
+	}
+	
+	public int getSomeLocalField2() {
+		return someLocalField;
+	}
+	
+	public static final strictfp synchronized String iterateOverFields() {
+		StringBuilder sb = new StringBuilder();
+		for(CrazyEnum crazyEnum : values()) {
+			sb.append(crazyEnum.getSomeLocalField());
+		}
+		return sb.toString();
+	}
+}
